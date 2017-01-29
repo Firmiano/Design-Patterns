50bdd39c332a42c329af975b708159c65c3c99e8
Guilherme firmiano <Guilherme firmiano>
2017-01-29 21:20:28 -0200

    Builder

---
+++ Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shop = new Shop();
+
+            VehicleBuilder builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Builder/Shop.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Builder
+{
+    public class Shop
+    {
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+}
